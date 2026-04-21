9e2c9bf4b34a1f42344cb1c0f01dfec299f05a8c
pooya <pooya.oladazimi@gmail.com>
2026-04-21 23:29:12 +0200

    add

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
+import dotenv
+import os
 
 app = FastAPI()
+
+dotenv.load_dotenv()
+
+db_user = os.environ.get("DB_USER")
+db_pass = os.environ.get("DB_PASS")
+db_name = os.environ.get("DB_NAME")
+db_endpoint = os.environ.get("DB_ENDPOINT")
 
 
 @app.get("/user")
 def user():
+    db_conn = f"postgresql://{db_user}:{db_pass}@{db_endpoint}:5432/{db_name}"
     return {"_result": "User says hello"}
 
 
